4bf026e68e4c213aa97678b9db58a59ed8519aee
phatlenguyen166 <phatlenguyen166@gmail.com>
2024-04-06 21:16:30 +0700

    update sql

--- web_2.sql
+++ web_2.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 09, 2024 lúc 12:04 PM
--- Phiên bản máy phục vụ: 10.4.32-MariaDB
--- Phiên bản PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2024 at 04:14 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,170 +18,215 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `web_2`
+-- Database: `web_2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `bill`
+-- Table structure for table `bill`
 --
 
 CREATE TABLE `bill` (
-  `id` int(11) NOT NULL,
-  `date_create` date NOT NULL,
-  `username` varchar(255) NOT NULL
+  `id_bill` int(11) NOT NULL,
+  `bill_status` int(1) NOT NULL DEFAULT 1,
+  `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id_bill`, `bill_status`, `id_user`) VALUES
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `bill_detail`
+-- Table structure for table `bill_detail`
 --
 
 CREATE TABLE `bill_detail` (
-  `id` int(11) NOT NULL,
-  `id_bill` int(11) NOT NULL,
-  `id_sanpham` int(11) NOT NULL,
-  `count` int(11) NOT NULL,
-  `thanhtien` int(11) NOT NULL
+  `id_billdetail` int(11) NOT NULL,
+  `soluongmua` int(100) NOT NULL,
+  `masp` int(11) NOT NULL,
+  `id_bill` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bill_detail`
+--
+
+INSERT INTO `bill_detail` (`id_billdetail`, `soluongmua`, `masp`, `id_bill`) VALUES
+(1, 2, 1, 1),
+(2, 4, 2, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `hangsanxuat`
+-- Table structure for table `hangsanxuat`
 --
 
 CREATE TABLE `hangsanxuat` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `id_hangsanxuat` int(11) NOT NULL,
+  `thutu` int(11) NOT NULL,
+  `tenhang` varchar(100) NOT NULL,
+  `hinhAnh` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `hangsanxuat`
+--
+
+INSERT INTO `hangsanxuat` (`id_hangsanxuat`, `thutu`, `tenhang`, `hinhAnh`) VALUES
+(1, 1, 'ACER', '1711348935_acer.png'),
+(2, 2, 'ASUS', '1711348987_asus.png'),
+(3, 3, 'DELL', '1711349000_dell.png'),
+(4, 4, 'LENOVO', '1711349013_lenovo.png'),
+(5, 5, 'MSI', '1711349027_msi.png');
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `sanpham`
+-- Table structure for table `sanpham`
 --
 
 CREATE TABLE `sanpham` (
-  `id` int(11) NOT NULL,
+  `masp` int(11) NOT NULL,
   `id_hangsanxuat` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `gia` int(11) NOT NULL,
-  `soluong` int(11) NOT NULL
+  `hinhAnh` varchar(100) NOT NULL,
+  `thutu` int(100) NOT NULL,
+  `tensp` varchar(255) NOT NULL,
+  `giaGoc` int(11) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `tinhtrang` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sanpham`
+--
+
+INSERT INTO `sanpham` (`masp`, `id_hangsanxuat`, `hinhAnh`, `thutu`, `tensp`, `giaGoc`, `soluong`, `tinhtrang`) VALUES
+(1, 4, '1711349162 lenovo-2.png', 1, 'LENOVO\r\n', 10000000, 12, 1),
+(2, 2, '1711349268 lenovo-2.png', 2, 'ASUS', 12000000, 11, 1);
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `diachi` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `dienthoai` varchar(255) NOT NULL,
   `type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Đang đổ dữ liệu cho bảng `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`username`, `password`, `type`) VALUES
-('a', '1', 1),
-('admin', 'admin', 0),
-('user1', 'user1', 1),
-('user2', 'a', 1);
+INSERT INTO `user` (`id_user`, `username`, `password`, `diachi`, `email`, `dienthoai`, `type`) VALUES
+(1, 'phatle', '123', 'e4/5d', 'phat@gmail.com', '0338641606', 1);
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `bill`
+-- Indexes for table `bill`
 --
 ALTER TABLE `bill`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `username` (`username`);
+  ADD PRIMARY KEY (`id_bill`) USING BTREE,
+  ADD KEY `id_user` (`id_user`);
 
 --
--- Chỉ mục cho bảng `bill_detail`
+-- Indexes for table `bill_detail`
 --
 ALTER TABLE `bill_detail`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_bill` (`id_bill`),
-  ADD KEY `id_sanpham` (`id_sanpham`);
+  ADD PRIMARY KEY (`id_billdetail`),
+  ADD KEY `billdetail_bill` (`id_bill`),
+  ADD KEY `billdetail_sp` (`masp`);
 
 --
--- Chỉ mục cho bảng `hangsanxuat`
+-- Indexes for table `hangsanxuat`
 --
 ALTER TABLE `hangsanxuat`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_hangsanxuat`);
 
 --
--- Chỉ mục cho bảng `sanpham`
+-- Indexes for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_hangsanxuat` (`id_hangsanxuat`);
+  ADD PRIMARY KEY (`masp`),
+  ADD KEY `FK_sanpham_hangsanxuat` (`id_hangsanxuat`);
 
 --
--- Chỉ mục cho bảng `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `bill`
+-- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_bill` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT cho bảng `bill_detail`
+-- AUTO_INCREMENT for table `bill_detail`
 --
 ALTER TABLE `bill_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_billdetail` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT cho bảng `hangsanxuat`
+-- AUTO_INCREMENT for table `hangsanxuat`
 --
 ALTER TABLE `hangsanxuat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_hangsanxuat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT cho bảng `sanpham`
+-- AUTO_INCREMENT for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `masp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `bill`
+-- Constraints for table `bill`
 --
 ALTER TABLE `bill`
-  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`username`) REFERENCES `user` (`username`);
+  ADD CONSTRAINT `bill_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 
 --
--- Các ràng buộc cho bảng `bill_detail`
+-- Constraints for table `bill_detail`
 --
 ALTER TABLE `bill_detail`
-  ADD CONSTRAINT `bill_detail_ibfk_1` FOREIGN KEY (`id_bill`) REFERENCES `bill` (`id`),
-  ADD CONSTRAINT `bill_detail_ibfk_2` FOREIGN KEY (`id_sanpham`) REFERENCES `sanpham` (`id`);
+  ADD CONSTRAINT `billdetail_bill` FOREIGN KEY (`id_bill`) REFERENCES `bill` (`id_bill`),
+  ADD CONSTRAINT `billdetail_sp` FOREIGN KEY (`masp`) REFERENCES `sanpham` (`masp`);
 
 --
--- Các ràng buộc cho bảng `sanpham`
+-- Constraints for table `sanpham`
 --
 ALTER TABLE `sanpham`
-  ADD CONSTRAINT `sanpham_ibfk_1` FOREIGN KEY (`id_hangsanxuat`) REFERENCES `hangsanxuat` (`id`);
+  ADD CONSTRAINT `FK_sanpham_hangsanxuat` FOREIGN KEY (`id_hangsanxuat`) REFERENCES `hangsanxuat` (`id_hangsanxuat`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
